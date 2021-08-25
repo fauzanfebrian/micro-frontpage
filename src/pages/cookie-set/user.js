db19accd1836fe6b3f7cdfe355c68f1c7878e76c
fauzanfebrian <fauzanfebrian1725@gmail.com>
2021-08-25 14:57:26 +0700

    oke

--- src/pages/cookie-set/user.js
+++ src/pages/cookie-set/user.js
@@ -3,14 +3,17 @@ import React, { useEffect } from "react";
 
 export default function user() {
   const router = useRouter();
-  const { name, thumbnail } = router.query;
+  const { name, thumbnail, path } = router.query;
 
   useEffect(() => {
-    const expires = new Date(new Date().getTime * 7 * 24 * 60 * 60 * 1000);
-    const userCookie = { name, thumbnail };
-    window.document.cookie = `BWAMICRO:user=${JSON.stringify(
-      userCookie
-    )}; expires=${expires.toUTCString()}; path=/;`;
+    if (name && thumbnail && path) {
+      const expires = new Date(new Date().getTime * 7 * 24 * 60 * 60 * 1000);
+      const userCookie = { name, thumbnail };
+      window.document.cookie = `BWAMICRO:user=${JSON.stringify(
+        userCookie
+      )}; expires=${expires.toUTCString()}; path=/;`;
+      window.location = `${process.env.NEXT_PUBLIC_MEMBERPAGE_HOST}${path}`;
+    }
   });
   return <div>Hello</div>;
 }
